bedaebf20b8145404c98576d5be729c3f2a3e268
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-06-30 22:57:38 +0100

    user model created

---
+++ models/userModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "User name required!"],
+    unique: [true, "User name already taken!"],
+  },
+  email: {
+    type: String,
+    required: [true, "User email required!"],
+    unique: [true, "User email already used!"],
+  },
+  password: {
+    type: String,
+    required: [true, "User password required!"],
+    minlength: [8, "Password must have at least 8 characters!"],
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+});
