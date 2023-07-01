5a4e31d385e5036de3ee0f91eedf548f6b8d0869
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-07-01 22:09:18 +0100

    blog route & controller modified

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -26,5 +26,20 @@ export const addBlog = async (req, res, next) => {
   } catch (err) {
     return console.log(err);
   }
+  return res.status(201).json({ blog });
+};
+
+export const getBlogById = async (req, res, next) => {
+  const id = req.params.id;
+  const blog = await Blog.findById(id);
+  if (!blog) {
+    return res.status(404).json({ message: "Blog not found!" });
+  }
   return res.status(200).json({ blog });
+};
+
+export const updateBlog = async (req, res, next) => {
+  const { title, description } = req.body;
+  const id = req.params.id;
+  const blog = await Blog.findByIdAndUpdate(id);
 };
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { getAllBlogs, addBlog } from "../controllers/blogController.js";
+import {
+  getAllBlogs,
+  addBlog,
+  getBlogById,
+} from "../controllers/blogController.js";
 
 const router = express.Router();
 
 router.get("/", getAllBlogs);
-
 router.post("/", addBlog);
+
+router.get("/:id", getBlogById);
+router.put("/:id");
 
 export default router;
