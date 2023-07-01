4b4ba57513df7c4ca54b0be2fa96b7352aad7b54
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-07-01 23:33:10 +0100

    blog controller modified

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -74,13 +74,14 @@ export const deleteBlog = async (req, res, next) => {
   const id = req.params.id;
   let blog;
   try {
-    blog = await Blog.findById(id);
+    blog = await Blog.findByIdAndRemove(id).populate("user");
+    await blog.user.blogs.pull(blog);
+    await blog.user.save();
   } catch (err) {
     return console.log(err);
   }
   if (!blog) {
     return res.status(404).json({ message: "Blog not found" });
   }
-  await Blog.findByIdAndDelete(id);
   return res.status(204).json({ message: "Blog has been deleted" });
 };
